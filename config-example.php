6899adb69820d8482e5ecaced6d5fc8b5a8e2b64
kensidiangco <kensidiangco@gmail.com>
2021-08-25 18:25:23 +0800

    make it better.

--- config-example.php
+++ config-example.php
@@ -5,6 +5,8 @@ define('DB_SERVER', 'your_server');
 define('DB_USERNAME', 'your_username');
 define('DB_PASSWORD', 'your_password');
 define('DB_NAME', 'yout_database');
+define('DB_NAME2', 'yout_database');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+$link2 = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME2);
